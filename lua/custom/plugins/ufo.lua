05ce21470a6dcf4329b15e6c61526672d503fc16
Paul Charmoille <paul.charmoille@unit-image.fr>
2024-07-08 13:54:39 +0200

    feat(ufo): Enable folding

---
+++ lua/custom/plugins/ufo.lua
@@ -0,0 +1,35 @@
+-- https://github.com/kevinhwang91/nvim-ufo
+
+vim.o.foldcolumn = "1" -- '0' is not bad
+vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
+
+return {
+	"kevinhwang91/nvim-ufo",
+	dependencies = { "kevinhwang91/promise-async" },
+	config = function()
+		require("ufo").setup({
+			preview = {
+				win_config = {
+					border = { "", "─", "", "", "", "─", "", "" },
+					winhighlight = "Normal:Folded",
+					winblend = 0,
+				},
+				mappings = {
+					scrollU = "<C-u>",
+					scrollD = "<C-d>",
+					jumpTop = "[",
+					jumpBot = "]",
+				},
+			},
+			provider_selector = function(bufnr, filetype, buftype)
+				-- refer to ./doc/example.lua for detail
+				return { "treesitter", "indent" }
+			end,
+		})
+		-- set keymaps
+		vim.keymap.set("n", "zR", require("ufo").openAllFolds)
+		vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+	end,
+}

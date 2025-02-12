5a16e6376240336081f1f102866830345fa07379
Paul Charmoille <paul.charmoille@unit-image.fr>
2025-02-12 20:38:19 +0100

    feat: Snippets

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -27,6 +27,7 @@ return { -- Autocompletion
 			},
 		},
 		"saadparwaiz1/cmp_luasnip",
+		"rafamadriz/friendly-snippets",
 
 		-- Adds other completion capabilities.
 		--  nvim-cmp does not ship with all sources by default. They are split
@@ -41,6 +42,8 @@ return { -- Autocompletion
 		local luasnip = require("luasnip")
 		luasnip.config.setup({})
 
+		require("luasnip.loaders.from_vscode").lazy_load()
+
 		cmp.setup({
 			snippet = {
 				expand = function(args)
@@ -54,29 +57,16 @@ return { -- Autocompletion
 			--
 			-- No, but seriously. Please read `:help ins-completion`, it is really good!
 			mapping = cmp.mapping.preset.insert({
-				-- Select the [n]ext item
-				["<C-n>"] = cmp.mapping.select_next_item(),
-				-- Select the [p]revious item
-				["<C-p>"] = cmp.mapping.select_prev_item(),
 
 				-- Scroll the documentation window [b]ack / [f]orward
 				["<C-b>"] = cmp.mapping.scroll_docs(-4),
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
 
-				-- Accept ([y]es) the completion.
-				--  This will auto-import if your LSP supports it.
-				--  This will expand snippets if the LSP sent a snippet.
 				["<CR>"] = cmp.mapping.confirm({ select = true }),
-
-				-- If you prefer more traditional completion keymaps,
-				-- you can uncomment the following lines
-				-- ['<CR>'] = cmp.mapping.confirm { select = true },
-				-- ['<Tab>'] = cmp.mapping.select_next_item(),
-				-- ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+				["<Tab>"] = cmp.mapping.select_next_item(),
+				["<S-Tab>"] = cmp.mapping.select_prev_item(),
 
 				-- Manually trigger a completion from nvim-cmp.
-				--  Generally you don't need this, because nvim-cmp will display
-				--  completions whenever it has completion options available.
 				["<C-Space>"] = cmp.mapping.complete({}),
 
 				-- Think of <c-l> as moving to the right of your snippet expansion.
@@ -97,16 +87,13 @@ return { -- Autocompletion
 						luasnip.jump(-1)
 					end
 				end, { "i", "s" }),
-
-				-- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-				--    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
 			}),
 			sources = {
-				{ name = "path" },
-				{ name = "buffer" },
+				-- { name = "path" },
+				-- { name = "buffer" },
 				{ name = "nvim_lsp" },
 				{ name = "nvim_lsp_signature_help" },
-				{ name = "diag-codes" },
+				-- { name = "diag-codes" },
 				{ name = "luasnip" },
 			},
 		})

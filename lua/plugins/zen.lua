5c4db7379e83aba5407e4244bd1167d25dd18968
Paul Charmoille <paul.charmoille@unit-image.fr>
2025-02-19 21:09:52 +0100

    fix(zen): Setup with options

--- lua/plugins/zen.lua
+++ lua/plugins/zen.lua
@@ -1,20 +1,20 @@
 -- Lua
 return {
 	"folke/zen-mode.nvim",
-	opts = {
-		window = {
-			width = 0.90,
-		},
-		plugins = {
-			options = {
-				laststatus = 3, -- turn on the statusline in zen mode
-			},
-			lualine = { enabled = true }, -- enable to start Twilight when zen mode opens
-		},
-	},
 	config = function()
+		local opts = {
+			window = {
+				width = 0.70,
+			},
+			plugins = {
+				options = {
+					laststatus = 3, -- turn on the statusline in zen mode
+				},
+				lualine = { enabled = true }, -- enable to start Twilight when zen mode opens
+			},
+		}
 		local zen_mode = require("zen-mode")
-		zen_mode.setup()
+		zen_mode.setup(opts)
 		vim.keymap.set("n", "<leader>z", function()
 			zen_mode.toggle()
 		end, { desc = "Zen mode" })
